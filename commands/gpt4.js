9d2e5f6d18f43bc5e6808ecadb070094ba713976
davbotapp <davbotapp@gmail.com>
2025-06-02 04:22:41 +0100

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,21 +1,53 @@
 const axios = require('axios');
-const { sendMessage } = require('../handles/sendMessage');
+const { sendMessage } = require('../handles/message');
+const MAX_MESSAGE_LENGTH = 2000;
+const DELAY_BETWEEN_MESSAGES = 1000; // 1 second
+
+
+function sendLongMessage(senderId, text, pageAccessToken, sendMessage) {
+  if (text.length > MAX_MESSAGE_LENGTH) {
+    const messages = splitMessageIntoChunks(text, MAX_MESSAGE_LENGTH);
+
+    sendMessage(senderId, { text: messages[0] }, pageAccessToken);
+
+    messages.slice(1).forEach((message, index) => {
+      setTimeout(() => sendMessage(senderId, { text: message }, pageAccessToken), (index + 1) * DELAY_BETWEEN_MESSAGES);
+    });
+  } else {
+    sendMessage(senderId, { text }, pageAccessToken);
+  }
+}
+
+
+function splitMessageIntoChunks(message, chunkSize) {
+  const regex = new RegExp(`.{1,${chunkSize}}`, 'g');
+  return message.match(regex);
+}
 
 module.exports = {
   name: 'gpt4',
-  description: 'Interact with GPT-4o',
-  usage: 'gpt4 [your message]',
+  description: 'Ask GPT-4 for a response to a given query',
+  usage: '-gpt4 <query>',
   author: 'coffee',
-
   async execute(senderId, args, pageAccessToken) {
-    const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+    if (!args || !Array.isArray(args) || args.length === 0) {
+      await sendMessage(senderId, { text: 'Please provide a query.' }, pageAccessToken);
+      return;
+    }
+
+    const query = args.join(' ');
 
     try {
-      const { data: { response } } = await axios.get(`https://api.kenliejugarap.com/blackbox-gpt4o/?text=${encodeURIComponent(prompt)}`);
-      sendMessage(senderId, { text: response }, pageAccessToken);
-    } catch {
-      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+      const apiUrl = `https://markdevs-last-api-2epw.onrender.com/api/v2/gpt4?query=${encodeURIComponent(query)}`;
+      const response = await axios.get(apiUrl);
+      const gptResponse = response.data.respond;
+
+      
+      sendLongMessage(senderId, gptResponse, pageAccessToken, sendMessage);
+
+    } catch (error) {
+      console.error('Error:', error);
+      await sendMessage(senderId, { text: 'Error: Could not get a response from GPT-4.' }, pageAccessToken);
     }
   }
 };
